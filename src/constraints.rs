abd4d656d8b38f362de8d41a2fe0e3fc93cdcf57
diannajonesa <154773988+diannajonesa@users.noreply.github.com>
2023-12-28 04:54:00 +0000

    整键重排

--- src/constraints.rs
+++ src/constraints.rs
@@ -135,7 +135,7 @@ impl Constraints {
         loop {
             let key = rng.gen_range(0..self.elements);
             if !self.fixed.contains(&key)
-                && !self.narrowed.contains_key(&key)
+                //&& !self.narrowed.contains_key(&key)
                 && !self.grouped.contains_key(&key)
             {
                 return key;
@@ -143,12 +143,62 @@ impl Constraints {
         }
     }
 
-    pub fn constrained_random_swap(&self, map: &KeyMap) -> KeyMap {
+    fn swap_narrowed_elements(&self, map: &KeyMap, element1: usize, element2: usize) -> KeyMap {
         let mut next = map.clone();
+        let destinations1 = self
+            .narrowed
+            .get(&element1)
+            .unwrap_or(&self.alphabet);
+        let destinations2 = self
+            .narrowed
+            .get(&element2)
+            .unwrap_or(&self.alphabet);
+        //分开判断可行性。这样如果无法交换，至少移动一下。
+        if destinations1.contains(&map[element2]) {
+            next[element1] = map[element2];
+        }
+        if destinations2.contains(&map[element1]) {
+            next[element2] = map[element1];
+        }
+        next
+    }
+
+    pub fn constrained_random_swap(&self, map: &KeyMap) -> KeyMap {
         let element1 = self.get_swappable_element();
         let element2 = self.get_swappable_element();
-        next[element1] = map[element2];
-        next[element2] = map[element1];
+        self.swap_narrowed_elements(map, element1, element2)
+    }
+
+    pub fn constrained_full_key_swap(&self, map: &KeyMap) -> KeyMap {
+        let mut rng = thread_rng();
+        let mut next = map.clone();
+        //寻找一个可移动元素。这样交换不成也至少能移动一次
+        let movable_element = self.get_movable_element();
+        let destinations = self
+            .narrowed
+            .get(&movable_element)
+            .unwrap_or(&self.alphabet);
+        let key = destinations
+            .choose(&mut rng)
+            .expect(&format!("元素 {} 无法移动", movable_element));
+        let former_key = map[movable_element];
+        for i in 0..map.len() {
+            if map[i] == former_key || map[i] == *key {
+                let mut destination = *key;
+                if map[i] == destination {
+                    destination = former_key;
+                }
+                //将元素移动到目标
+                //考虑到组合中的元素必然在同样的键上，有同样的约束条件，也必然跟随移动，这里不再判断组合
+                let destinations2 = self
+                    .narrowed
+                    .get(&i)
+                    .unwrap_or(&self.alphabet);
+                if destinations2.contains(&destination) {
+                    next[i] = destination;
+                }
+            }
+        }
         next
     }
 
