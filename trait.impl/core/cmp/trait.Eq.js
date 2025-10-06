8bb1dc9ddb2d7ed08a4b657572f55ee3f978e533
tansongchen <tansongchen@users.noreply.github.com>
2025-10-06 12:51:35 +0000

    deploy: 5e1a361525cf25cb222a7c4b3ff185f866922579

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["chai",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"chai/config/enum.Mapped.html\" title=\"enum chai::config::Mapped\">Mapped</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"chai/config/enum.MappedKey.html\" title=\"enum chai::config::MappedKey\">MappedKey</a>"]]]]);
+    var implementors = Object.fromEntries([["chai",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"chai/config/enum.Mapped.html\" title=\"enum chai::config::Mapped\">Mapped</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"chai/config/enum.MappedKey.html\" title=\"enum chai::config::MappedKey\">MappedKey</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
