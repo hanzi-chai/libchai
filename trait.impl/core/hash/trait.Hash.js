1eb799ad880f9a114173b4f5c3a37ce3cc5cf529
tansongchen <tansongchen@users.noreply.github.com>
2026-02-15 18:09:21 +0000

    deploy: 2cfaceadbc03750b66c2f98a5789fd7a7e7ce471

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["chai",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"chai/config/enum.安排.html\" title=\"enum chai::config::安排\">安排</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"chai/config/enum.广义码位.html\" title=\"enum chai::config::广义码位\">广义码位</a>"]]]]);
+    var implementors = Object.fromEntries([["chai",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"chai/config/enum.安排.html\" title=\"enum chai::config::安排\">安排</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"chai/config/enum.广义码位.html\" title=\"enum chai::config::广义码位\">广义码位</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
